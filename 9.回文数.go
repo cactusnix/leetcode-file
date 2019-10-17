6b19024d64afe7bb27ca6fa89f4aa46051a9efd4
cactusnix <cocalemontea@gmail.com>
2019-10-17 15:09:46 +0800

    add 9

---
+++ 9.回文数.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=golang
+ *
+ * [9] 回文数
+ *
+ * https://leetcode-cn.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (56.66%)
+ * Likes:    772
+ * Dislikes: 0
+ * Total Accepted:    181.5K
+ * Total Submissions: 320.3K
+ * Testcase Example:  '121'
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 121
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 
+ * 
+ * 进阶:
+ * 
+ * 你能不将整数转为字符串来解决这个问题吗？
+ * 
+ */
+
+// @lc code=start
+func isPalindrome(x int) bool {
+		// 考虑特殊值 0
+    if x < 0 || (x % 10 == 0 && x != 0) {
+			return false
+		}
+		revertedNum := 0
+		for x > revertedNum {
+			revertedNum = revertedNum * 10 + x % 10
+			x /= 10
+		}
+		// 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+		// 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+		// 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+		return x == revertedNum || x == revertedNum / 10
+}
+// @lc code=end
+
+// @Tips
+/*
+方案一
+倒序转成字符串，进行比较
+方案二
+取一半的数进行判断
+*/
